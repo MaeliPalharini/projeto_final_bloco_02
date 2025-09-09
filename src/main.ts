c390b6a79632a2718d1fcea4894e778546325637
Maeli <maeli.palharini@hotmail.com>
2025-09-09 12:13:18 -0300

    fix: ajusta main para receber front

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const app = await NestFactory.create(AppModule);
 
-    await app.listen(Number(process.env.APP_PORT) || 4000);
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(Number(process.env.APP_PORT) || 4000);
 }
 bootstrap();
 
